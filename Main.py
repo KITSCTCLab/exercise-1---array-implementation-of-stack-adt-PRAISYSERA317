6d05288aead2a5ac979bbd29200734b0b2b2d8f1
URK21CS1199-PRAISYSERAFINA <109951858+URK21CS1199-PRAISYSERAFINA@users.noreply.github.com>
2022-07-25 15:00:54 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,19 +3,28 @@ class Stack:
     def __init__(self, size):
         self.items = []
         self.size = size
+        self.top=-1
 
     def is_empty(self):
         # Write code here
+        return self.top==-1
 
     def is_full(self):
         # Write code here
+        return self.top==self.n-1
 
     def push(self, data):
         if not self.is_full():
+            self.top+=1
+            x=int(input("Enter data to be inserted: "))
+            self.st[self.top]=x
             # Write code here
 
     def pop(self):
         if not self.is_empty():
+            self.top=-1
+            y=int(input("Enter data to be deleted: "))
+            self.st[self.top]=y
             # Write code here
 
     def status(self):
