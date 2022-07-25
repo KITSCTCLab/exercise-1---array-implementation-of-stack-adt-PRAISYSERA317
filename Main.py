b347b8abe6368cedef7eb20bb4ca0d669026e99a
URK21CS1199-PRAISYSERAFINA <109951858+URK21CS1199-PRAISYSERAFINA@users.noreply.github.com>
2022-07-25 16:06:35 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,34 +3,24 @@ class Stack:
     def __init__(self, size):
         self.items = []
         self.size = size
-        self.top=-1
 
     def is_empty(self):
-        # Write code here
-        return self.top==-1
+        return len(self.items) == 0
 
     def is_full(self):
-        # Write code here
-        return self.top==self.n-1
+        return len(self.items) == size
 
     def push(self, data):
         if not self.is_full():
-            self.top+=1
-            x=int(input("Enter data to be inserted: "))
-            self.size[self.top]=x
-            # Write code here
+            self.items.append(values[1])
 
     def pop(self):
         if not self.is_empty():
-            self.top=-1
-            y=int(input("Enter data to be deleted: "))
-            self.size[self.top]=y
-            # Write code here
+            self.items.pop()
 
     def status(self):
-        # Write code here
-        for i in range(self.top+1):
-            print(self.size[i])
+        for i in self.items:
+            print(i)
 
 # Do not change the following code
 size, queries = map(int, input().rstrip().split())
@@ -42,3 +32,4 @@ for line in range(queries):
     elif values[0] == 2:
         stack.pop()
 stack.status()
+
